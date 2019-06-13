bdf27139c9872f07d1b8cde104078d6b8105746d
Werik Rodrigues <werikrodrigues1@hotmail.com.br>
2019-06-13 19:51:49 -0300

    Adicionando função de soma

---
+++ projeto.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int soma(int a, int b){
+	
+	int soma_num = a+ b;
+	return soma_num;
+	
+}
+int main(){
+	
+	printf ("Soma dos dois numeros: %d \n", soma(5,5));
+	return 0;
+}
